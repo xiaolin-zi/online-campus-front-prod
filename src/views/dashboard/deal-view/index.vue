a88586e08efa48b58924182f324b3b83fa0e7fc8
xiaolin <1849569695@qq.com>
2023-10-11 10:40:46 +0800

    第一次部署

--- src/views/dashboard/deal-view/index.vue
+++ src/views/dashboard/deal-view/index.vue
@@ -9,7 +9,7 @@
           <div class="title">{{ item.description }}</div>
           <div class="features">{{item.categoryName}} | {{item.subCategoryName }}</div>
           <div class="price">￥{{ item.price }}</div>
-          <img :src="item.userAvatar" alt=""><span class="userName">{{item.userName}}</span>
+          <img :src="item.userAvatar" alt="">&nbsp;&nbsp; {item.userName}}
         </div>
 
       </div>
